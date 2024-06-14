2a80b6ec44041584900fdae5dc199b9aa06e400a
Pabalberione <pabalberione@gmail.com>
2024-06-14 13:47:29 -0300

    First commit

---
+++ src/test/java/com/api_empleados/ApiEmpleadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_empleados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmpleadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
